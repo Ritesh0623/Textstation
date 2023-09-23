396c4f20d81fecaed4dd59a2d5a203ea069c588d
Ritesh <rkp111213+1@gmail.com>
2023-09-23 22:09:04 +0530

    dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
 
 function App() {
   return (
     <>
       <Navbar title="Text Station" about="About Us" />
       <TextForm heading="Enter Text" />
+      <About />
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-    const [text, setText] = useState()
+    const [text, setText] = useState('')
 
     const upperCase = () => {
         let newText = text.toUpperCase();
@@ -9,6 +9,10 @@ export default function TextForm(props) {
     }
     const lowerCase = () => {
         let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const clearText = () => {
+        let newText = '';
         setText(newText);
     }
     const caseOnchange = (event) => {
@@ -22,7 +26,15 @@ export default function TextForm(props) {
                 <textarea className="form-control" value={text} onChange={caseOnchange} id='myText' rows="8"></textarea>
                 <button className="btn btn-primary my-2 mx-2" onClick={upperCase}>Convert to Uppercase</button>
                 <button className="btn btn-primary my-2 mx-2" onClick={lowerCase}>Convert to Lowercase</button>
+                <button className="btn btn-primary my-2 mx-2" onClick={clearText}>Clear</button>
             </div>
+        </div>
+        <div className="container my3">
+            <h2>Text Summary</h2>
+            <p>{text.split(" ").length} words and {text.length} character in your summary.</p>
+            <p>{0.008 * text.split(" ").length} minutes to read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
         </div>
         </>
     )
